a9fd6c5300272f4954521f09522e773e2f26355e
Felipe Melo <felipe.camargo@luizalabs.com>
2020-09-24 21:16:16 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 //responsavel para instanciar os Daos
 public class DaoFactory {
 
 	public static SellerDao createSellerDao(){ //retorna o tipo da interface com a variavel CreateSeller
-		return new SellerDaoJDBC(); 
+		return new SellerDaoJDBC(DB.getConnection()); 
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
 
+	private Connection conn;
+	
+	public SellerDaoJDBC (Connection conn) {   //construtor para realizar a conexao
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -26,9 +39,45 @@ public class SellerDaoJDBC implements SellerDao{
 	}
 
 	@Override
-	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Seller findById(Integer id) { //selects para buscar dados no banco
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.* ,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id=?");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			//transformar os dados do SQL para orientado a objeto para gravar na memoria
+			if (rs.next()) { //retorna se a consulta retornou alguma informação
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));//pegamos o id
+				dep.setName(rs.getString("DepName"));//pegamos o nome do departamento
+				
+				Seller obj = new Seller();
+				obj.setID(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthdate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep); //é DEP pois instanciamos um Department e passará todos os dados informados
+				
+				return obj;
+				
+			}
+			return null;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.CloseResultSet(rs); //fechando os recursos
+			DB.CloseStatement(st);
+		}
+		
 	}
 
 	@Override
