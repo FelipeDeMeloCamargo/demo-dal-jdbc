746e6744a3770e78269d115390214e125eeedf94
Felipe Melo <felipe.camargo@luizalabs.com>
2020-09-28 21:27:29 -0300

    Dele Implementation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,6 +2,7 @@ package aplication;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerdao = DaoFactory.createSellerDao();// instanciar um sellerDao que recebe o que vem da
 															// DaoFactory, independentemente do que seja.
 
@@ -38,11 +40,18 @@ public class Program {
 		sellerdao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getID());//retornará o id do new seller
 
-		System.out.println("\n=====Teste 5: Seller Insert =====");
+		System.out.println("\n=====Teste 5: Seller Update =====");
 		seller = sellerdao.findById(1);//carrega os dados do id 1
 		seller.setName("Martha Wayne");
 		sellerdao.update(seller);
 		System.out.println("Update complete");
+		
+		System.out.println("\n=====Teste 5: Seller Delete =====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerdao.deleteById(id);
+		System.out.println("Delete complete!");
+	
+	sc.close();	
 	}
-
 }
